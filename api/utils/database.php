a2d17471249f618ae58ef52d0e60a321fb593f16
Kayanagi <delordwesley@gmail.com>
2025-04-25 14:02:58 +0200

    wesley

--- api/utils/database.php
+++ api/utils/database.php
@@ -6,8 +6,8 @@ function getDatabaseConnection(): PDO {
         $host = 'localhost';
         $db = 'vikings';
         $user = 'root';
-        $pass = 'root';
-        $port = '8889';
+        $pass = '';
+        $port = '3306';
         return new PDO("mysql:host=$host;dbname=$db;port=$port", $user, $pass);
     } catch (PDOException $e) {
         returnError(500, 'Could not connect to the database. ' . $e->getMessage());
